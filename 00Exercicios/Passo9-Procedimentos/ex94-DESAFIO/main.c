3c42ee109c248be4e47e2d548b06db692db659ca
Lih Yasumura <97624980+lihyasumura@users.noreply.github.com>
2022-09-02 12:34:10 -0300

    c

--- 00Exercicios/Passo9-Procedimentos/ex94-DESAFIO/main.c
+++ 00Exercicios/Passo9-Procedimentos/ex94-DESAFIO/main.c
@@ -3,7 +3,9 @@
 #include <locale.h>
 #include <string.h>
 
+void Fibonacci();
 char R[] = "S";
+int T;
 
 int main()
 {
@@ -19,20 +21,27 @@ int main()
         printf ("QUANTOS TERMOS DA SEQUÊNCIA FIBONNACI VOCÊ QUER VER? ");
         scanf ("%d", &T);
         printf ("\n");
-        printf ("ALTERAR O NÚMERO DE TERMOS DA SEQUÊNCIA [ S/N ]? ");
+        Fibonacci(T);
+        printf ("\n\nALTERAR O NÚMERO DE TERMOS DA SEQUÊNCIA [ S/N ]? ");
         scanf ("%s", R);
         strupr(R);
     } while(strcmp(R, "S") == 0);
+    if (strcmp(R, "N") == 0) {
+        printf ("ATÉ A PRÓXIMA...");
+    }
     printf ("\n");
     return 0;
 }
 
 void Fibonacci(int T) {
-int c = 0, c1 = 0;
+int c = 0, c1 = 0, F = 1, F1 = 0, Res = 0;
     for (c = 0; c < T; c++) {
         if ((c1 % 5 == 0) && (c != 0)) {
             printf ("\n");
         }
+        Res = F + F1;
+        F = F1;
+        F1 = Res;
         printf ("[ %2d ]", Res);
         c1++;
     }
